d3b02ca141fad0eb6711f7d5c983c45b8524cab2
Aditya Bhandari <adityab@vmware.com>
2017-06-11 12:34:13 -0700

    [NG] Added Receiver to the Window

--- src/app/components/chat-window/chat-window.component.ts
+++ src/app/components/chat-window/chat-window.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {UserService} from "../../services/user.service";
+import {Subject} from "rxjs/Subject";
+import {Observable} from "rxjs/Observable";
 
 @Component({
   selector: 'app-chat-window',
@@ -12,21 +14,42 @@ export class ChatWindowComponent implements OnInit {
   messages: string[] = [];
 
   /*
-   * ID of this chat window's owner. This could be used to get info
+   * ID of this chat window's sender. This could be used to get info
    * like user details etc.
    * Ideally this would be at the Chat App level and each Chat Window would
    * request it but for this exercise since our browser has the sender and the receiver
    * at the same level, I am bring it down to the ChatWindow level.
    */
-  private _ownerId: number;
+  private _senderId: number;
 
-  get ownerId(): number {
-    return this._ownerId;
+  get senderId(): number {
+    return this._senderId;
   }
 
   @Input()
-  set ownerId(value: number) {
-    this._ownerId = value;
+  set senderId(value: number) {
+    if (typeof value === "number") {
+      this._senderId = value;
+    }
+  }
+
+  private _receiverId: number;
+
+  get receiverId(): number {
+    return this._receiverId;
+  }
+
+  @Input()
+  set receiverId(value: number) {
+    if (typeof value === "number") {
+      this._receiverId = value;
+    }
+  }
+
+  messenger: Subject<String>;
+
+  get receiver(): Observable<String> {
+    return this.messenger.asObservable();
   }
 
   constructor(private userService: UserService) {
