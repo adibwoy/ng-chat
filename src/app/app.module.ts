dfa58d3bf7631381117e9854a38706a07ddfa50b
Aditya Bhandari <adityab@vmware.com>
2017-06-11 11:34:18 -0700

    [NG] Intial Chat Window

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ChatWindowComponent } from './components/chat-window/chat-window.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatWindowComponent
   ],
   imports: [
     BrowserModule
