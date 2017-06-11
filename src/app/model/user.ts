c4a8fd378fafc81a03dbfaa13e15e5ae02ee2133
Aditya Bhandari <adityab@vmware.com>
2017-06-11 12:29:40 -0700

    [NG] Get User ability from UserService

--- src/app/model/user.ts
+++ src/app/model/user.ts
@@ -1,3 +1,8 @@
+
+/*
+  Ideally this class would also contain the Friend/Contact list which contains details about them
+  but I am skipping that for this exercise
+ */
 export class User {
   id: number;
   name: string;
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,12 +1,41 @@
 import {Injectable} from '@angular/core';
+import {User} from "../model/user";
+
+const USER_LIST: User[] = [
+  {
+    id: 1,
+    name: "Rob"
+  },
+  {
+    id: 2,
+    name: "Laura"
+  }
+];
+
 
 @Injectable()
 export class UserService {
 
-  getUserInfo(): void {
+  userList: User[] = [];
+
+  getUserInfo(id: number): User {
+    const users: User[] = this.userList.filter(user => user.id === id);
+    if (users.length > 0) {
+      return users[0];
+    } else {
+      return null;
+    }
   }
 
   constructor() {
+    this.populateUserList();
+  }
+
+  /**
+   *This would ideally be populated from a database but for now we have hardcoded lists
+   */
+  populateUserList() {
+    this.userList = USER_LIST;
   }
 
 }
