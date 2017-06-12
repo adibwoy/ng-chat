0e55a2bf00a4a3c174b84073cf490bd40ac3abdb
Aditya Bhandari <adityab@vmware.com>
2017-06-11 17:19:49 -0700

    Chat App

--- src/app/model/user.ts
+++
@@ -1,9 +0,0 @@
-
-/*
-  Ideally this class would also contain the Friend/Contact list which contains details about them
-  but I am skipping that for this exercise
- */
-export class User {
-  id: number;
-  name: string;
-}
